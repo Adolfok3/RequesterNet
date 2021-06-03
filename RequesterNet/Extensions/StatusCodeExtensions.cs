e03010b1f11fbe1d32bff7cf82dac8e94fdbbf19
Luiz Adolfo <luiz.adolfo@uds.com.br>
2021-06-03 11:12:38 -0300

    Implemented unity testes and feature ci.

--- RequesterNet/Extensions/StatusCodeExtensions.cs
+++ RequesterNet/Extensions/StatusCodeExtensions.cs
@@ -4,69 +4,19 @@ namespace RequesterNetLib.Extensions
 {
     public static class StatusCodeExtensions
     {
-        public static bool IsOk(this HttpStatusCode statusCode)
-        {
-            return statusCode == HttpStatusCode.OK;
-        }
-
-        public static bool IsCreated(this HttpStatusCode statusCode)
-        {
-            return statusCode == HttpStatusCode.Created;
-        }
-
-        public static bool IsAccepted(this HttpStatusCode statusCode)
-        {
-            return statusCode == HttpStatusCode.Accepted;
-        }
-
-        public static bool IsNoContent(this HttpStatusCode statusCode)
-        {
-            return statusCode == HttpStatusCode.NoContent;
-        }
-
-        public static bool IsBadRequest(this HttpStatusCode statusCode)
-        {
-            return statusCode == HttpStatusCode.BadRequest;
-        }
-
-        public static bool IsUnauthorized(this HttpStatusCode statusCode)
-        {
-            return statusCode == HttpStatusCode.Unauthorized;
-        }
-
-        public static bool IsForbidden(this HttpStatusCode statusCode)
-        {
-            return statusCode == HttpStatusCode.Forbidden;
-        }
-
-        public static bool IsNotFound(this HttpStatusCode statusCode)
-        {
-            return statusCode == HttpStatusCode.NotFound;
-        }
-
-        public static bool IsInternalServerError(this HttpStatusCode statusCode)
-        {
-            return statusCode == HttpStatusCode.InternalServerError;
-        }
-
-        public static bool IsServiceUnavailable(this HttpStatusCode statusCode)
-        {
-            return statusCode == HttpStatusCode.ServiceUnavailable;
-        }
-
-        public static bool IsGatewayTimeout(this HttpStatusCode statusCode)
-        {
-            return statusCode == HttpStatusCode.GatewayTimeout;
-        }
-
-        public static bool IsBadGateway(this HttpStatusCode statusCode)
-        {
-            return statusCode == HttpStatusCode.BadGateway;
-        }
-
-        public static bool IsHttpVersionNotSupported(this HttpStatusCode statusCode)
-        {
-            return statusCode == HttpStatusCode.HttpVersionNotSupported;
-        }
+        public static bool IsOk(this HttpStatusCode statusCode) => statusCode == HttpStatusCode.OK;
+        public static bool IsCreated(this HttpStatusCode statusCode) => statusCode == HttpStatusCode.Created;
+        public static bool IsAccepted(this HttpStatusCode statusCode) => statusCode == HttpStatusCode.Accepted;
+        public static bool IsNoContent(this HttpStatusCode statusCode) => statusCode == HttpStatusCode.NoContent;
+        public static bool IsBadRequest(this HttpStatusCode statusCode) => statusCode == HttpStatusCode.BadRequest;
+        public static bool IsUnauthorized(this HttpStatusCode statusCode) => statusCode == HttpStatusCode.Unauthorized;
+        public static bool IsForbidden(this HttpStatusCode statusCode) => statusCode == HttpStatusCode.Forbidden;
+        public static bool IsNotFound(this HttpStatusCode statusCode) => statusCode == HttpStatusCode.NotFound;
+        public static bool IsUnprocessableEntity(this HttpStatusCode statusCode) => statusCode == HttpStatusCode.UnprocessableEntity;
+        public static bool IsInternalServerError(this HttpStatusCode statusCode) => statusCode == HttpStatusCode.InternalServerError;
+        public static bool IsServiceUnavailable(this HttpStatusCode statusCode) => statusCode == HttpStatusCode.ServiceUnavailable;
+        public static bool IsGatewayTimeout(this HttpStatusCode statusCode) => statusCode == HttpStatusCode.GatewayTimeout;
+        public static bool IsBadGateway(this HttpStatusCode statusCode) => statusCode == HttpStatusCode.BadGateway;
+        public static bool IsHttpVersionNotSupported(this HttpStatusCode statusCode) => statusCode == HttpStatusCode.HttpVersionNotSupported;
     }
 }
---
+++ RequesterNetLib.Test/RequesterNetTest.cs
@@ -0,0 +1,108 @@
+﻿using RequesterNetLib.Extensions;
+using RequesterNetLib.Options;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RequesterNetLib.Test
+{
+    public class RequesterNetTest
+    {
+        [Fact]
+        public async Task GetAsyncShouldBeOk()
+        {
+            var requester = new RequesterNet();
+            var response = await requester.GetAsync("https://jsonplaceholder.typicode.com/posts/1");
+
+            Assert.True(response.StatusCode.IsOk());
+        }
+
+        [Fact]
+        public async Task GetAsyncWithUrlBaseShouldBeOk()
+        {
+            var options = Microsoft.Extensions.Options.Options.Create(new RequesterNetOptions
+            {
+                UrlBase = "https://jsonplaceholder.typicode.com"
+            });
+            var requester = new RequesterNet(options);
+            var response = await requester.GetAsync("todos/1");
+
+            Assert.True(response.StatusCode.IsOk());
+        }
+
+        [Fact]
+        public async Task GetAsyncWithDefaultHeadersShouldBeOk()
+        {
+            var options = Microsoft.Extensions.Options.Options.Create(new RequesterNetOptions
+            {
+                UrlBase = "https://jsonplaceholder.typicode.com",
+                DefaultHeaders = new Dictionary<string, string>
+                {
+                    { "client_id", Guid.NewGuid().ToString("N") }
+                }
+            });
+            var requester = new RequesterNet(options);
+            var response = await requester.GetAsync("todos/1");
+
+            Assert.True(response.StatusCode.IsOk());
+        }
+
+        [Fact]
+        public async Task PostShouldBeOk()
+        {
+            var body = new
+            {
+                userId = 1,
+                id = 1,
+                title = "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+                body = "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+            };
+            var requester = new RequesterNet();
+            var response = await requester.PostAsync("https://jsonplaceholder.typicode.com/posts", body: body);
+
+            Assert.True(response.StatusCode.IsCreated());
+        }
+
+        [Fact]
+        public async Task PutShouldBeOk()
+        {
+            var body = new
+            {
+                userId = 1,
+                id = 1,
+                title = "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+                body = "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+            };
+            var requester = new RequesterNet();
+            var response = await requester.PutAsync("https://jsonplaceholder.typicode.com/posts/1", body: body);
+
+            Assert.True(response.StatusCode.IsOk());
+        }
+
+        [Fact]
+        public async Task PatchShouldBeOk()
+        {
+            var body = new
+            {
+                userId = 1,
+                id = 1,
+                title = "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+                body = "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+            };
+            var requester = new RequesterNet();
+            var response = await requester.PatchAsync("https://jsonplaceholder.typicode.com/posts/1", body: body);
+
+            Assert.True(response.StatusCode.IsOk());
+        }
+
+        [Fact]
+        public async Task DeleteShouldBeOk()
+        {
+            var requester = new RequesterNet();
+            var response = await requester.DeleteAsync("https://jsonplaceholder.typicode.com/posts/1");
+
+            Assert.True(response.StatusCode.IsOk());
+        }
+    }
+}
